6888308772bd653b818b318a82f9c2d05d5e43c8
Paul Jordaan <pdjordaan@gmail.com>
2015-09-04 18:55:10 +0200

    Added LCD setup

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 var five = require("johnny-five");
 var board = new five.Board();
-var startButtonPin = 2;
-var redLEDPin = 3; // has to be digital, I think
-var greenLEDPin = 4; // ditto
-var servoPin = 5; // should probably be pwm pin (~ or # on board?)
+var startButtonPin = 10;
+var redLEDPin = 11; // has to be digital, I think
+var greenLEDPin = 12; // ditto
+
+/**
+ * Servo
+ */
+var servoPin = 9; // should probably be pwm pin (~ or # on board?)
+
+
 
 // create the story file and start the story
 var fs = require('fs');
@@ -12,8 +18,12 @@ fs.writeFile("story.txt", "This is the story: \n", function(err) {
 		return console.log(err);
 	}
 });
-	
+
+/**
+ * Start the process
+ */
 board.on("ready", function() {
+
 
 	console.log('ready!');
 
@@ -25,6 +35,17 @@ board.on("ready", function() {
 	greenLED.off();
 	var servo = new five.Servo(servoPin);
 	servo.min();
+
+	/**
+	 *  LCD
+	 */
+	 var lcd = new five.LCD({
+	   // LCD pin name  RS  EN  DB4 DB5 DB6 DB7
+	   // Arduino pin # 12  11   5   4  3  2
+	   pins: [ 7, 6, 5, 4, 3, 2 ],
+	   rows: 2,
+	   cols: 16
+	 });
 
 	var min = function(){
 		servo.min();
@@ -54,8 +75,12 @@ board.on("ready", function() {
 			// make coffee!
 			servo.max();
 			// wait x seconds:
-			setTimeout(min,1000); 
-			greenLED.off()
+			setTimeout(min,5000); 
+			sdfgs(sd, function(){
+
+			})
+
+			 greenLED.off()
 
 		} else {
 
@@ -68,7 +93,7 @@ board.on("ready", function() {
 		}
 		// reset components
 		redLED.off();
-		greenLED.off();
-		servo.min();
+		// greenLED.off();
+		// servo.min();
 	});
 });
