ad91638ff06fcefbca212221861ec001506ed855
Phlippie Bosman <phlippie.bosman@gmail.com>
2015-09-05 13:55:45 +0200

    test.js om te werk sonder die arduino stuff

---
+++ test.js
@@ -0,0 +1,11 @@
+var fs = require('fs')
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+fs.watch('smiles', function(event, filename) {
+	// test if new file is created? hang af wat die processing stuff doen i guess
+	if (event == 'rename') {
+		io.emit('showForm');
+	}
+});
